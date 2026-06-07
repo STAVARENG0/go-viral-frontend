2f4492bf5b17f93ba544ee123eacfd2d32284f96
Go Viral <services.go.viral@gmail.com>
2026-06-07 19:36:10 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || './',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: false
   }
 });
